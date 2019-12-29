7c4314fa722048750588e3c244f2b800aa31e35c
howarto <howarto@ymail.com>
2019-12-29 09:24:33 +0100

    Added TypeScript definitions

---
+++ src/displacejs.d.ts
@@ -0,0 +1,41 @@
+declare module 'displacejs' {
+  export default function displace(element: HTMLElement, options?: DisplaceJSOptions): DisplaceJSObject;
+}
+
+type DisplaceJSObject = {
+  // Runs setup again. Useful when divs have been moved or resized.
+  reinit: function(): void,
+  // Removes event listeners and destroys instance.
+  destroy: function(): void
+};
+
+type DisplaceJSOptions = {
+  // Constrains element to its parent container.
+  constrain?: boolean,
+
+  // Constrains element to the specified DOM element. Requires constrain to be true.
+  relativeTo?: HTMLElement,
+
+  // Assigns a child element as the moveable handle for the parent element.
+  handle?: null,
+
+  // Allows you to highlight text in inputs and textareas by disabling drag
+  // events originating from those elements.
+	highlightInputs?: false,
+
+	// Interaction events.
+	onMouseDown?: DisplaceJSEvent,
+	onMouseMove?: DisplaceJSEvent,
+	onMouseUp?: DisplaceJSEvent,
+	onTouchStart?: DisplaceJSEvent,
+	onTouchMove?: DisplaceJSEvent,
+	onTouchStop?: DisplaceJSEvent,
+
+  // Function that can be used to override how x and y are being set on the
+  // displaced element on move.
+	customMove?: DisplaceJSMove
+};
+
+type DisplaceJSEvent = (element: HTMLElement) => void;
+
+type DisplaceJSMove = (element: HTMLElement, xMovement: number, yMovement: number) => void;
